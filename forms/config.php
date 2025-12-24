dbbc00e024f352e3f3103cabff79731769329358
Robb Robb <justrobb@gmail.com>
2025-12-24 09:58:32 -0500

    Contact form description & security

--- forms/config.php
+++ forms/config.php
@@ -2,7 +2,7 @@
 // config.php - Secure storage for sensitive values
 // Keep this file out of GitHub (.gitignore it!)
 return [
-    'smtp_password' => 'CHEWgroup5!',  // Your SMTP password here
-    'turnstile_secret' => '0x4AAAAAACID5hNU3-JYF5nVS0vaPKD5CEs'
+    'smtp_password' => '?W4o`@550EH:',  // Your SMTP password here
+    'turnstile_secret' => '0x4AAAAAACID5ig5LSC4GQvNC9sd_WHUM-U'
 ];
 ?>
