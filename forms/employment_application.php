7b0fc1f07ca9b63422c2ca6903d78b42448413c8
Robb Robb <justrobb@gmail.com>
2026-01-21 13:42:04 -0500

    Fpdi fix

--- forms/employment_application.php
+++ forms/employment_application.php
@@ -6,6 +6,10 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+require_once __DIR__ . '/../vendor/tecnickcom/tcpdf/tcpdf.php';
+require_once __DIR__ . '/../vendor/setasign/fpdi/src/autoload.php';
+use setasign\Fpdi\Tcpdf\Fpdi;
+
 // Load .env from root (one level up from /forms/)
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
